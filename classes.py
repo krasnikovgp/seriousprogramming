5be097a92f59fb4736a6276ce83d8bdd4e0532c5
krasnikovgp <148316952+krasnikovgp@users.noreply.github.com>
2023-10-18 10:01:20 +0300

    Добавил первый класс (Student)

---
+++ classes.py
@@ -0,0 +1,42 @@
+class Student:
+    '''
+    Класс для представления силы человека.
+    '''
+
+    def __init__(self, name, power, age, sex='М'):
+        self.name = name
+        self.sex = sex
+        self.power = power
+        self.age = age
+        self.is_beated = False
+
+    def fight(self, other):
+        if other.is_beated:
+            print(f'{other.name} уже побит!')
+        elif self.is_beated:
+            print(f'{self.name} уже побит!')
+        if other.power > self.power:
+            print(f'{other.name} побил {self.name}!')
+            self.is_beated = True
+        elif other.power < self.power:
+            print(f'{self.name} побил {other.name}!')
+            other.is_beated = True
+        if self.sex == 'Ж' or other.sex == 'Ж':
+            print('Вы не можете драться!')
+
+    def greeting(self, other):
+        if self.age < other.age:
+            print(f'Здравствуйте, {other.name}!')
+        else:
+            print('Привет, шкет!')
+
+
+human1 = Student('Гоша', 8, 16)
+human2 = Student('Никита', 5, 16)
+human3 = Student('Марь Ивановна', -10, 99)
+human4 = Student('Глеб', 3, 11)
+
+human1.greeting(human3)
+human1.fight(human3)
+human1.greeting(human4)
+human1.fight(human2)
