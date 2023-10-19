f243e6f39ae042622bd895835d6e93c515a46290
krasnikovgp <148316952+krasnikovgp@users.noreply.github.com>
2023-10-19 19:52:46 +0300

    небольшой шаг к подклассам
    ничего не получается, сделал заготовку

---
+++ classes2.py
@@ -0,0 +1,20 @@
+from classes import Student
+
+
+class Boxer(Student):
+    """Представление боксера в БД"""
+
+
+class Bodybuilder(Student):
+    """Представление качка в БД"""
+    pass
+
+
+class Nerd(Student):
+    """Представление ботана в БД"""
+    pass
+
+
+class Girl(Student):
+    """Представление девочки в БД"""
+    pass
