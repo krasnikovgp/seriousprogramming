7dc02a067994077586f47d5c64fedf1d09401bac
krasnikovgp <148316952+krasnikovgp@users.noreply.github.com>
2023-10-20 20:19:57 +0300

    Добавил и изменил пару команд
    Добавил команду /help, изменил клавиатуру на /menu, сделал заготовку для /clear и сделал ответ для неизвестных команд.

--- telebot.py
+++ telebot.py
@@ -1,7 +1,7 @@
 from telegram import Update, ReplyKeyboardRemove, ReplyKeyboardMarkup
 from telegram.ext import *
 import logging
-from settings import TOKEN
+from key import TOKEN
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,10 +16,14 @@ def main():
 
     echo_handler = MessageHandler(Filters.text, do_echo)
     start_handler = CommandHandler('start', do_start)
-    keyboard_handler = CommandHandler('keyboard', do_keyboard)
+    menu_handler = CommandHandler('menu', do_menu)
+    help_handler = CommandHandler('help', do_help)
+    unknown_handler = MessageHandler(Filters.command, unknown)
 
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(keyboard_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(menu_handler)
+    dispatcher.add_handler(unknown_handler)
     dispatcher.add_handler(echo_handler)
 
     updater.start_polling()
@@ -37,7 +41,7 @@ def do_echo(update: Update, context: CallbackContext):
         f'Твой {username=}',
         f'Твой id = {user_id}',
         f'Ты написал "{text}"'
-        ]
+    ]
     answer = '\n'.join(answer)
 
     update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
@@ -50,23 +54,47 @@ def do_start(update: Update, context: CallbackContext):
     text = [
         f'Здарова, {user_fullname}!',
         f'У меня теперь есть твой {user_id=}',
-        'Я знаю команды /start, /keyboard'
-        ]
+        'Напиши /menu, чтобы узнать, что я умею :)'
+    ]
     text = '\n'.join(text)
-    update.message.reply_text(text)
+    update.message.reply_text(text, reply_markup=ReplyKeyboardRemove())
 
 
-def do_keyboard(update: Update, context: CallbackContext):
+def do_menu(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
-    logger.info(f'{user_id=} вызвал клавиатуру')
+    logger.info(f'{user_id=} вызвал меню')
     buttons = [
-        ['one', 'two'],
+        ['/help', 'some'],
         ['weather in Moscow'],
         ['real time in USA']
     ]
     text = 'Выберите кнопку :)'
     keyboard = ReplyKeyboardMarkup(buttons)
     update.message.reply_text(text, reply_markup=keyboard)
+
+
+def do_help(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    logger.info(f'{user_id=} вызвал команду do_help')
+    text = [
+        f'У меня есть разные команды:',
+        f'/menu (остальное в разработке)',
+        f'Также у меня есть функция ECHO'
+    ]
+    text = '\n'.join(text)
+    update.message.reply_text(text, reply_markup=ReplyKeyboardRemove())
+
+
+"""
+def do_clear(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    logger.info(f'{user_id=} вызвал команду do_clear')
+"""
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Друг, у меня такого нет :(")
 
 
 if __name__ == '__main__':
