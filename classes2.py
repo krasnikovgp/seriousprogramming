1de0025a785aff94ba193d64bb092f020d9e1c63
krasnikovgp <148316952+krasnikovgp@users.noreply.github.com>
2023-10-20 19:29:36 +0300

    Закончил subclasses

--- classes2.py
+++ classes2.py
@@ -3,7 +3,7 @@ from classes import Student
 
 class Male(Student):
     def __init__(self, name, power, age):
-        super().__init__(name, power, age)
+        super().__init__(name, power, age, 'М')
 
 
 class Female(Student):
@@ -12,16 +12,20 @@ class Female(Student):
 
 
 class StrongMale(Student):
-    def __init__(self, name, age, power=10):
-        super().__init__(name, age, 'М')
+    def __init__(self, name, power, age):
+        super().__init__(name, power, age, 'М')
 
 
 class StrongFemale(Student):
-    def __init__(self, name, age, power=10):
-        super().__init__(name, age, 'Ж')
+    def __init__(self, name, power, age):
+        super().__init__(name, power, age, 'Ж')
 
 
 person1 = Male('Тема', 4, 18)
 person2 = Female('Маша', 2, 18)
-person3 = StrongMale('Гриша', 20)
-person4 = StrongFemale('Женя', 17)
+person3 = StrongMale('Гриша', 10, 20)
+person4 = StrongFemale('Женя', 9, 17)
+
+person1.fight(person3)
+person4.greeting(person2)
+person1.greeting(person4)
